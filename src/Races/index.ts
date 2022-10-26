c91288530498b4154db7416123067324c64c6b9b
Alex-Horman-de-Medeiros-Correia <alexhorman1990@gmail.com>
2022-10-25 23:41:28 -0300

    fazendo o req 1 e req 2

---
+++ src/Races/index.ts
@@ -0,0 +1,9 @@
+import Race from './Race';
+import Orc from './Orc';
+import Elf from './Elf';
+import Dwarf from './Dwarf';
+import Halfling from './Halfling';
+
+export default Race;
+
+export { Elf, Halfling, Orc, Dwarf };
